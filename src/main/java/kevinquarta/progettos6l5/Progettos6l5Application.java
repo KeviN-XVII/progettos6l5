894e02f2360541c48e3fca76f9e2a013082ba69a
KeviN-XVII <kevinquartar@gmail.com>
2026-02-13 10:00:00 +0100

    Initial commit

---
+++ src/main/java/kevinquarta/progettos6l5/Progettos6l5Application.java
@@ -0,0 +1,13 @@
+package kevinquarta.progettos6l5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progettos6l5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progettos6l5Application.class, args);
+	}
+
+}
---
+++ src/test/java/kevinquarta/progettos6l5/Progettos6l5ApplicationTests.java
@@ -0,0 +1,13 @@
+package kevinquarta.progettos6l5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progettos6l5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
